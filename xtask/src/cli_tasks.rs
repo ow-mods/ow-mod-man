39b7dcab82ef97418dfd6948452c8e3afb294652
Ben C <bwc9876@gmail.com>
2023-04-15 17:37:31 -0400

    [ALL] Update Package And Release Structure

---
+++ xtask/src/cli_tasks.rs
@@ -0,0 +1,55 @@
+use std::fs::{create_dir_all, File};
+use std::io::Write;
+
+use anyhow::Result;
+use clap::CommandFactory;
+use clap_mangen::Man;
+
+use crate::{get_out_dir, get_pkg_version};
+
+include!("../../owmods_cli/src/cli.rs");
+
+pub fn generate_man_files() -> Result<()> {
+    let out_dir = get_out_dir()?.join("cli").join("man");
+    create_dir_all(&out_dir)?;
+    let man = Man::new(BaseCli::command());
+    let mut buffer: Vec<u8> = Default::default();
+    man.render(&mut buffer)?;
+    std::fs::write(out_dir.join("owmods.1"), buffer)?;
+    let cmd = BaseCli::command();
+    for subcommand in cmd.get_subcommands() {
+        let subcommand = subcommand.clone();
+        let subcommand_name = format!("owmods-{}", subcommand.get_name());
+        let mut buffer: Vec<u8> = Default::default();
+        let man = Man::new(subcommand.clone().name(&subcommand_name));
+        man.render(&mut buffer)?;
+        std::fs::write(
+            std::path::PathBuf::from(&out_dir).join(format!("{}.1", &subcommand_name)),
+            buffer,
+        )?;
+    }
+    Ok(())
+}
+
+pub fn generate_completions() -> Result<()> {
+    let out_dir = get_out_dir()?.join("cli").join("completions");
+    create_dir_all(&out_dir)?;
+    let shells: Vec<Shell> = vec![Shell::Zsh, Shell::Bash, Shell::Fish];
+    for shell in shells {
+        let mut cmd = BaseCli::command();
+        let shell_name = format!("{:?}", shell).to_ascii_lowercase();
+        let mut file = File::create(out_dir.join(format!("owmods.{}", shell_name)))?;
+        clap_complete::generate(shell, &mut cmd, "owmods", &mut file);
+    }
+    Ok(())
+}
+
+pub fn generate_pkg_build() -> Result<()> {
+    let version = get_pkg_version(include_str!("../../owmods_cli/Cargo.toml"));
+    let pkgbuild = include_str!("cli_templates/PKGBUILD").replace("~~VERSION~~", version);
+    let out_dir = get_out_dir()?.join("cli").join("pkgbuild");
+    create_dir_all(&out_dir)?;
+    let mut file = File::create(out_dir.join("PKGBUILD"))?;
+    write!(file, "{}", pkgbuild)?;
+    Ok(())
+}
---
+++ xtask/src/main.rs
@@ -0,0 +1,42 @@
+use std::path::PathBuf;
+
+use anyhow::Result;
+use cli_tasks::{generate_completions, generate_man_files, generate_pkg_build};
+use regex::RegexBuilder;
+
+mod cli_tasks;
+
+pub fn get_out_dir() -> Result<PathBuf> {
+    let out_dir = std::path::PathBuf::from(std::env::var_os("CARGO_MANIFEST_DIR").unwrap())
+        .parent()
+        .unwrap()
+        .join("dist");
+    Ok(out_dir)
+}
+
+pub fn get_pkg_version(in_str: &str) -> &str {
+    let re = RegexBuilder::new(r#"^version = "(.*?)"$"#)
+        .multi_line(true)
+        .build()
+        .unwrap();
+    let group = re.captures_iter(in_str).next().unwrap();
+    let version_match = group.get(1).unwrap();
+    version_match.as_str()
+}
+
+fn main() -> Result<()> {
+    let cmd = std::env::args().nth(1).expect("Missing Command Name");
+    match cmd.as_str() {
+        "gen_man" => generate_man_files()?,
+        "gen_completions" => generate_completions()?,
+        "dist_cli" => {
+            println!("Generating Completions...");
+            generate_completions()?;
+            println!("Generating Man Pages...");
+            generate_man_files()?;
+        }
+        "cli_pkg_build" => generate_pkg_build()?,
+        _ => panic!("Invalid Command: {cmd}"),
+    }
+    Ok(())
+}

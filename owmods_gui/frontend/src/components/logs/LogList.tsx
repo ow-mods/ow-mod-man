06ee4de8c8ac7672574d64a0eeee8e7634272610
Ben C <bwc9876@gmail.com>
2023-04-23 14:27:02 -0400

    [GUI] Disable smooth scroll on follow output, increase atBottom threshold

--- owmods_gui/frontend/src/components/logs/LogList.tsx
+++ owmods_gui/frontend/src/components/logs/LogList.tsx
@@ -18,7 +18,8 @@ const LogList = memo((props: LogListProps) => {
             computeItemKey={(index) => `${index}-${props.logLines[index][0]}`}
             data={props.logLines}
             itemContent={(_, data) => <LogLine port={props.port} line={data[0]} count={data[1]} />}
-            followOutput="smooth"
+            atBottomThreshold={100}
+            followOutput
             alignToBottom
         />
     );

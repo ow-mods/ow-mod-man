d6e212ca0997ff955b66a7c1c5f516518986fc2b
Ben C <bwc9876@gmail.com>
2023-04-23 17:34:19 -0400

    [GUI] Try Fix Log Auto Scroll

--- owmods_gui/frontend/src/components/logs/LogList.tsx
+++ owmods_gui/frontend/src/components/logs/LogList.tsx
@@ -1,7 +1,8 @@
-import { memo } from "react";
+import { memo, useRef } from "react";
 import { LogFilter } from "./LogApp";
 import LogLine from "./LogLine";
 import { Virtuoso } from "react-virtuoso";
+import { VirtuosoHandle } from "react-virtuoso";
 
 export interface LogListProps {
     port: number;
@@ -11,13 +12,23 @@ export interface LogListProps {
 }
 
 const LogList = memo((props: LogListProps) => {
+    const virtuoso = useRef<VirtuosoHandle | null>(null);
+
     return (
         <Virtuoso
+            ref={virtuoso}
             className="log-list"
             increaseViewportBy={5000}
             computeItemKey={(index) => `${index}-${props.logLines[index][0]}`}
             data={props.logLines}
-            itemContent={(_, data) => <LogLine port={props.port} line={data[0]} count={data[1]} />}
+            itemContent={(_, data) => (
+                <LogLine
+                    scrollToBottom={virtuoso.current?.autoscrollToBottom}
+                    port={props.port}
+                    line={data[0]}
+                    count={data[1]}
+                />
+            )}
             atBottomThreshold={100}
             followOutput
             alignToBottom

7f43a4d0b2b3774075e164af8e11f1c202e117e2
Ben C <bwc9876@gmail.com>
2023-06-21 09:52:25 -0400

    [GUI] Fix CSP In Prod

--- owmods_gui/frontend/src/main.tsx
+++ owmods_gui/frontend/src/main.tsx
@@ -2,11 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@components/main/MainApp";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     <React.StrictMode>
         <App />

077c527a08e210fd7180499b86bb8b3f169636df
Ben C <bwc9876@gmail.com>
2023-06-20 21:21:39 -0400

    [GUI] Add GUI Config Migration

--- owmods_gui/backend/src/gui_config.rs
+++ owmods_gui/backend/src/gui_config.rs
@@ -40,10 +40,6 @@ const fn _default_false() -> bool {
 #[derive(Serialize, Deserialize, Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct GuiConfig {
-    #[serde(default = "Theme::default")]
-    theme: Theme,
-    #[serde(default = "_default_false")]
-    rainbow: bool,
     #[serde(default = "Language::default")]
     language: Language,
     #[serde(default = "_default_true")]
@@ -56,13 +52,18 @@ pub struct GuiConfig {
     auto_enable_deps: bool,
     #[serde(default = "_default_false")]
     pub no_log_server: bool,
+    // Old
+    #[serde(skip_serializing_if = "Option::is_none")]
+    theme: Option<Theme>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    rainbow: Option<bool>,
 }
 
 impl Default for GuiConfig {
     fn default() -> Self {
         Self {
-            theme: Theme::default(),
-            rainbow: false,
+            theme: None,
+            rainbow: None,
             language: Language::default(),
             watch_fs: true,
             no_warning: false,
@@ -79,8 +80,21 @@ impl GuiConfig {
         Ok(path)
     }
 
+    fn migrate(&mut self) -> bool {
+        if self.theme.is_some() || self.rainbow.is_some() {
+            self.theme = None;
+            self.rainbow = None;
+            return true;
+        }
+        false
+    }
+
     fn read() -> Result<Self, anyhow::Error> {
-        deserialize_from_json::<GuiConfig>(&Self::path()?)
+        let mut conf = deserialize_from_json::<GuiConfig>(&Self::path()?)?;
+        if conf.migrate() {
+            conf.save()?;
+        }
+        Ok(conf)
     }
 
     fn write(config: &Self) -> Result<(), anyhow::Error> {

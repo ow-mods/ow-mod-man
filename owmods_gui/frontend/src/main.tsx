67a5bd6e21f5cf5dcebeea226f7e36b022c19084
Raicuparta <Raicuparta@users.noreply.github.com>
2023-09-28 17:40:55 +0100

    Disable ctrl+p for print dialogue

--- owmods_gui/frontend/src/main.tsx
+++ owmods_gui/frontend/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import { ErrorBoundary } from "react-error-boundary";
 import { basicFallbackRender, onError } from "./errorHandling";
 
+// Prevent ctrl+p from opening the print dialog
+document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key === "p") {
+	    e.preventDefault();
+    }
+});
+
 const App = React.lazy(() => import("@components/main/MainApp"));
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(

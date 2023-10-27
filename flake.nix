3a0dd87e0f0dc0d7cb9e7ed2a24a585361b50ee3
Locochoco <ivanrwpf@gmail.com>
2023-10-26 23:34:34 -0300

    adding flake with modules and overlay

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Flake for owmods-cli and owmods-gui";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/master";
+  };
+
+  outputs = { self, flake-utils, nixpkgs }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = (import nixpkgs) {
+          inherit system;
+	  overlays = [ self.overlay.owmods ];
+        };
+      in rec {
+        # For `nix build` & `nix run`:
+        packages = rec {
+          owmods-cli = pkgs.owmods-cli;
+          owmods-gui = pkgs.owmods-gui;
+          default = pkgs.owmods-cli;
+        };        
+        # For `nix develop`:
+        #devShell = pkgs.mkShell {
+        #  nativeBuildInputs = with pkgs; [ rustc cargo openssl libsoup ];
+        #};
+      }
+    ) // {
+        overlay.owmods = import ./nix/overlay.nix;
+	nixosModules.owmods = import ./nix/modules/nixos.nix;
+	homeManagerModules.owmods = import ./nix/modules/hm.nix;
+    };
+}
---
+++ nix/owmods-gui.nix
@@ -0,0 +1,47 @@
+{ stdenv
+, lib
+, dpkg
+, fetchurl
+, autoPatchelfHook
+, glib-networking
+, openssl_1_1
+, webkitgtk
+, wrapGAppsHook
+}:
+
+stdenv.mkDerivation rec {
+  pname = "owmods-gui";
+  version = "0.11.3";
+
+  src = fetchurl {
+    url = "https://github.com/ow-mods/ow-mod-man/releases/download/gui_v${version}/outer-wilds-mod-manager_${version}_amd64.deb";
+    hash = "sha256-D+rnF40e1xpwKeMvyiOuhMSJ1+JlDKq5c195iam3d2Y=";
+  };
+
+  nativeBuildInputs = [
+    autoPatchelfHook
+    dpkg
+  ];
+
+  buildInputs = [
+    glib-networking
+    openssl_1_1
+    webkitgtk
+    wrapGAppsHook
+  ];
+
+  unpackCmd = "dpkg-deb -x $curSrc source";
+
+  installPhase = "mv usr $out";
+  meta = with lib; {
+    description = "GUI version of the mod manager for Outer Wilds Mod Loader";
+    homepage = "https://github.com/ow-mods/ow-mod-man/tree/main/owmods_gui";
+    downloadPage = "https://github.com/ow-mods/ow-mod-man/releases/tag/gui_v${version}";
+    changelog = "https://github.com/ow-mods/ow-mod-man/releases/tag/gui_v${version}";
+    mainProgram = "outer-wilds-mod-manager";
+    sourceProvenance = with sourceTypes; [ binaryNativeCode ];
+    platforms = platforms.linux;
+    license = licenses.gpl3;
+    maintainers = with maintainers; [ locochoco ];
+  };
+}

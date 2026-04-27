c164647ebeeb5b6d992b2d6e8ee8e500ecfe0df8
Ben C <ben@bwc9876.dev>
2026-04-27 12:39:49 -0400

    [CLI] Canonicalize OWML path when running `owml setup`

--- owmods_cli/src/main.rs
+++ owmods_cli/src/main.rs
@@ -82,7 +82,8 @@ async fn run_from_cli(cli: BaseCli) -> Result<()> {
         } => {
             if let Some(owml_path) = owml_path {
                 let mut new_config = config.clone();
-                new_config.owml_path = owml_path.to_str().unwrap().to_string();
+                let path  = owml_path.canonicalize().context("Failed to resolve OWML path")?.to_str().unwrap().to_string();
+                new_config.owml_path = path;
                 if new_config.check_owml() {
                     info!("Path to OWML is valid! Updating config...");
                     new_config.save()?;
